536176837b5c2455d841385e780a479f13d18b41
anuarf99 <anuarf.99@gmail.com>
2019-08-07 13:30:09 +0000

    save

--- bin/Controller.js
+++ bin/Controller.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const User = require("./models/User");
-const Product = require("./models/Product");
 const News = require("./models/News");
 const Forums = require("./models/Forums");
+const Products = require("./models/Products");
 
 class Controller{
     constructor(){
@@ -20,18 +20,26 @@ class Controller{
         }
     }
 
-
-
         //metodo para buscar todos los usuarios
-    getUsers(res){
+    getUsers(req, res){
         //en el modelo User se ejecuta el find sin ninguna condicion...
         User.find({}, (err, users)=>{
         //en caso de haberse presentado un error se ejecuta el error
             if(err) throw err;
         //de lo contrario se retorna un objeto con todos los resultados
-            res.send( users );
+            res.send({ allUsers: users });
         })
     }
+
+    getUser(id, res) {
+    //en el modelo User se ejecuta el find sin ninguna condicion...
+    User.find({ _id: id }, (err, user) => {
+      //en caso de haberse presentado un error se ejecuta el error
+      if (err) throw err;
+      //de lo contrario se retorna un objeto con todos los resultados
+      res.send({ User: user });
+    });
+  }
 
     postUsers(req, res){
         //en el modelo User se ejecuta el find sin ninguna condicion...
@@ -44,48 +52,48 @@ class Controller{
         })
     }
 
+    updateUser(user, res) {
+    //optenemos los campos que queremos actualizar
+    let { id, nickname, password, picture } = user;
+    //actualizamos teniendo en cuenta una condicion con el operador $set
+    //https://docs.mongodb.com/manual/reference/operator/update/set/
+    User.updateOne(
+      { _id: id },
+      { $set: { picture: picture, password: password, nickname:nickname } }
+    )
+      .then(rawResponse => {
+        res.send({ message: "User updated", raw: rawResponse });
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
+
+
+    deleteUser(id, res) {
+    User.deleteOne({ _id: id }, function(err) {
+      if (err) throw err;
+      res.send({ message: "User has been deleted" });
+    });
+  }
 
 
 
- getProducts(res){
-        Product.find({}, (err, products)=>{
-            if(err) throw err;
-            res.send( products );
-        })
-    }
-
-    postProducts(req, res){
-        let products = req.body.products;
-        Product.create( products, (err, result)=>{
-            if(err)throw err;
-            res. send({newProducts:result})
-        })
-    }
-
-
-getNews(res){
-        News.find({}, (err, news)=>{
-            if(err) throw err;
-            res.send( news );
-        })
-    }
-
-    postNews(req, res){
-        let news = req.body.news;
-        News.create( news, (err, result)=>{
-            if(err)throw err;
-            res. send({newNews:result})
-        })
-    }
-
-getForums(res){
+    getForums(req, res){
         Forums.find({}, (err, forums)=>{
             if(err) throw err;
-            res.send( forums );
+            res.send({ allForums: forums });
         })
     }
 
-postForums(req, res){
+    getForum(id, res) {
+    Forums.find({ _id: id }, (err, newss) => {
+      if (err) throw err;
+      res.send({ Forum: forum });
+    });
+  }
+
+    postForums(req, res){
         let forums = req.body.forums;
         Forums.create( forums, (err, result)=>{
             if(err)throw err;
@@ -93,7 +101,72 @@ postForums(req, res){
         })
     }
 
+    updateForums(forums, res) {
+    let { id, thematic } = forums;
+    News.updateOne(
+      { _id: id },
+      { $set: { thematic: thematic } }
+    )
+      .then(rawResponse => {
+        res.send({ message: "Forums updated", raw: rawResponse });
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
 
+
+    deleteNews(id, res) {
+    Forums.deleteOne({ _id: id }, function(err) {
+      if (err) throw err;
+      res.send({ message: "This News has been deleted" });
+    });
+  }
+
+
+getProducts(req, res){
+        Products.find({}, (err, products)=>{
+            if(err) throw err;
+            res.send({ allProducts: products });
+        })
+    }
+
+    getProduct(id, res) {
+    Products.find({ _id: id }, (err, product) => {
+      if (err) throw err;
+      res.send({ Product: product });
+    });
+  }
+
+    postProducts(req, res){
+        let products = req.body.products;
+        Products.create( products, (err, result)=>{
+            if(err)throw err;
+            res. send({newProducts:result})
+        })
+    }
+
+    updateProducts(user, res) {
+    let { id, name, date, place_of_creation, type, route } = user;
+    Products.updateOne(
+      { _id: id },
+      { $set: { name: name, date: date, place_of_creation: place_of_creation, type: type, route: route } }
+    )
+      .then(rawResponse => {
+        res.send({ message: "User updated", raw: rawResponse });
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
+
+
+    deleteProducts(id, res) {
+    Products.deleteOne({ _id: id }, function(err) {
+      if (err) throw err;
+      res.send({ message: "Products has been deleted" });
+    });
+  }
 
 
 }
--- bin/routes.js
+++ bin/routes.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) =>{
 
 app.get("/users", (req, res) =>{
 
-    controller.getUsers(res);
+    controller.getUsers(req, res);
 
 })
 
@@ -20,22 +20,27 @@ app.post("/users", (req, res)=>{
     controller.postUsers(req, res);
 })
 
+app.get("/users/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getUser(id, res);
+});
+
+app.put("/users/:id", function(req, res) {
+  let user = req.body.user;
+  user.id = req.params.id;
+  controller.updateUser(user, res);
+});
 
 
-app.get("/products", (req, res) =>{
+app.delete("/users/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteUser(id, res);
+});
 
-    controller.getProducts(res);
-
-})
-
-app.post("/products", (req, res)=>{
-    //console.log(req.body);
-    controller.postProducts(req, res);
-})
 
 app.get("/news", (req, res) =>{
 
-    controller.getNews(res);
+    controller.getNews(req, res);
 
 })
 
@@ -44,10 +49,56 @@ app.post("/news", (req, res)=>{
     controller.postNews(req, res);
 })
 
+app.get("/news/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getNewss(id, res);
+});
+
+app.put("/news/:id", function(req, res) {
+  let news = req.body.news;
+  news.id = req.params.id;
+  controller.updateNews(news, res);
+});
+
+
+app.delete("/news/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteNews(id, res);
+});
+
+
+app.get("/news", (req, res) =>{
+
+    controller.getNews(req, res);
+
+})
+
+app.post("/news", (req, res)=>{
+    //console.log(req.body);
+    controller.postNews(req, res);
+})
+
+app.get("/news/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getNewss(id, res);
+});
+
+app.put("/news/:id", function(req, res) {
+  let news = req.body.news;
+  news.id = req.params.id;
+  controller.updateNews(news, res);
+});
+
+
+app.delete("/news/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteNews(id, res);
+});
+
 
 app.get("/forums", (req, res) =>{
 
-    controller.getForums(res);
+    controller.getForums(req, res);
 
 })
 
@@ -56,6 +107,49 @@ app.post("/forums", (req, res)=>{
     controller.postForums(req, res);
 })
 
+app.get("/forums/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getForum(id, res);
+});
+
+app.put("/forums/:id", function(req, res) {
+  let forums = req.body.forums;
+  forums.id = req.params.id;
+  controller.updateUser(forums, res);
+});
 
 
+app.delete("/forums/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteForums(id, res);
+});
+
+
+app.get("/products", (req, res) =>{
+
+    controller.getProducts(req, res);
+
+})
+
+app.post("/products", (req, res)=>{
+    //console.log(req.body);
+    controller.postProducts(req, res);
+})
+
+app.get("/products/:id", function(req, res) {
+  let { id } = req.params;
+  controller.getProduct(id, res);
+});
+
+app.put("/products/:id", function(req, res) {
+  let products = req.body.products;
+  products.id = req.params.id;
+  controller.updateProducts(products, res);
+});
+
+
+app.delete("/products/:id", function(req, res) {
+  let { id } = req.params;
+  controller.deleteProducts(id, res);
+});
 exports.app = app;
